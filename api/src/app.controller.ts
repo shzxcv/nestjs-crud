f2b65fa6d1ff326c5a85614c5bd43e074b069e75
shzxcv <sh.zxcv00@gmail.com>
2022-08-18 22:26:21 +0900

    add

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -14,8 +14,18 @@ import {Post as PostModel} from '@prisma/client'
 export class AppController {
   constructor(private readonly postService: PostService) {}
 
+  @Get('posts')
+  async getPosts(): Promise<PostModel[]> {
+    return this.postService.getPosts()
+  }
+
+  @Get('post/:id')
+  async getPostById(@Param('id') id: string): Promise<PostModel> {
+    return this.postService.getPost({id: Number(id)})
+  }
+
   @Post('post')
-  async create(
+  async createPost(
     @Body() postData: {title: string; content?: string},
   ): Promise<PostModel> {
     const {title, content} = postData;
@@ -23,5 +33,21 @@ export class AppController {
       title,
       content,
     });
+  }
+
+  @Put('post/:id')
+  async updatePost(
+    @Param('id') id: string,
+    @Body() updateData: {title: string, content?: string},
+  ): Promise<PostModel> {
+    return this.postService.updatePost({
+      where: {id: Number(id)},
+      data: updateData,
+    })
+  }
+
+  @Delete('post/:id')
+  async deletePost(@Param('id') id: string): Promise<PostModel> {
+    return this.postService.deletePost({id: Number(id)});
   }
 }
--- api/src/post.service.ts
+++ api/src/post.service.ts
@@ -11,5 +11,31 @@ export class PostService {
       data,
     });
   }
-}
 
+  async getPost(postWhereUniqueInput: Prisma.PostWhereUniqueInput): Promise<Post | null> {
+    return this.prisma.post.findUnique({
+      where: postWhereUniqueInput,
+    });
+  }
+
+  async getPosts(): Promise<Post[] | null> {
+    return this.prisma.post.findMany();
+  }
+
+  async updatePost(params: {
+    where: Prisma.PostWhereUniqueInput;
+    data: Prisma.PostUpdateInput;
+  }): Promise<Post> {
+    const {data, where} = params;
+    return this.prisma.post.update({
+      data,
+      where,
+    });
+  }
+
+  async deletePost(where: Prisma.PostWhereUniqueInput): Promise<Post> {
+    return this.prisma.post.delete({
+      where,
+    });
+  }
+}
